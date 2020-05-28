9fec3a5633fffa556b69115bdbaeb9c5b1d35b60
Borko Tomić <borko.tomic@avawomen.com>
2020-05-28 16:27:26 +0200

    Added networking manger and loadData method

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -10,5 +10,28 @@ import Foundation
 extension RazeCore {
     public struct Networking {
         
+        /// Responsible for handling all networking calls
+        /// - Warning:   Must create before using any public API
+        public class Manager {
+            public init() {
+                
+            }
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { data, response, error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completion(result)
+                }
+                
+                task.resume()
+            }
+        }
+        
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
 }
