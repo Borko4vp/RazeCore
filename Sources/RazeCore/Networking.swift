3a8c651a66bcb854691b3554c86c9271cfa82d23
Borko Tomić <borko.tomic@avawomen.com>
2020-05-28 16:01:26 +0200

    Namespaces separated (Color, Networking)

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,14 @@
+//
+//  RazeNetworking.swift
+//  RazeCore
+//
+//  Created by Borko Tomic on 28/05/2020.
+//
+
+import Foundation
+
+extension RazeCore {
+    public struct Networking {
+        
+    }
+}
---
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -0,0 +1,21 @@
+//
+//  RazeNetworkingTests.swift
+//  RazeCoreTests
+//
+//  Created by Borko Tomic on 28/05/2020.
+//
+
+import XCTest
+@testable import RazeCore
+
+final class RazeNetworkingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    static var allTests = [
+        ("testExample", testExample)
+    ]
+        
+}
