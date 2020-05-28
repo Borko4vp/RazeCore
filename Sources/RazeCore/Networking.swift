4fd5656f985e55798aa1839e3e91afc47525266e
Borko Tomić <borko.tomic@avawomen.com>
2020-05-28 17:49:40 +0200

    Dependency injection on networking API

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,25 +7,40 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+             completion(data, error)
+         }
+         task.resume()
+    }
+    
+    
+}
+
 extension RazeCore {
     public struct Networking {
         
         /// Responsible for handling all networking calls
         /// - Warning:   Must create before using any public API
         public class Manager {
-            public init() {
-                
-            }
+            public init() {}
             
-            private let session = URLSession.shared
+            internal var session: NetworkSession?
             
+            /// Calls the live internet to retrieve Data from specific localtion
+            /// - Parameters:
+            ///   - url: the location you wish to fetch data from
+            ///   - completion: completion to execute after data is fetched
             public func loadData(from url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) { data, response, error in
+                session?.get(from: url) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completion(result)
                 }
-                
-                task.resume()
             }
         }
         
